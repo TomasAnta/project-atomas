f575e25a9dc741e56808a69635bc177dfe4841e2
TomasAnt <hi@atomas.io>
2023-07-28 16:16:49 +0300

    navigation

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -1,18 +1,27 @@
 import Link from "next/link"
+import { useTranslation } from "next-i18next"
+
+import { Icon } from "@components/core/icon/Icon"
 
 import { NavContainer, NavLinks, NavLogo } from "./navigation.styled"
 
-const Navigation = () => (
-  <NavContainer>
-    <NavLogo>Logo</NavLogo>
-    <NavLinks>
-      <Link href="/">Home</Link>
-      <Link href="/">Portfolio</Link>
-      <Link href="/">CV</Link>
-      <Link href="/">Skills</Link>
-      <Link href="/">Contact</Link>
-    </NavLinks>
-  </NavContainer>
-)
+const Navigation = () => {
+  const { t } = useTranslation()
+  return (
+    <NavContainer>
+      <NavLogo>
+        <Icon iconName="logo" />
+      </NavLogo>
+      <NavLinks>
+        <Link href="/">{t("navigation.homePage")}</Link>
+        <Link href="#portfolio">{t("navigation.portfolio")}</Link>
+        <Link href="#cv">{t("navigation.resume")}</Link>
+        <Link href="#about-me">{t("navigation.aboutMe")}</Link>
+        <Link href="#skills">{t("navigation.skills")}</Link>
+        <Link href="#contact">{t("navigation.contact")}</Link>
+      </NavLinks>
+    </NavContainer>
+  )
+}
 
 export default Navigation
