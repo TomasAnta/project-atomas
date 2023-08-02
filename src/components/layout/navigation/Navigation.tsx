ea740b23ab301d018fa9ed65283ccef46d316dc5
TomasAnt <hi@aomas.io>
2023-08-02 16:12:11 +0300

    language-switcher-i18n

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -1,27 +1,24 @@
-import Link from "next/link"
-import { useTranslation } from "next-i18next"
-
 import { Icon } from "@components/core/icon/Icon"
+import LanguageSwitcher from "@components/core/languageSwitcher/LanguageSwitcher"
 
 import { NavContainer, NavLinks, NavLogo } from "./navigation.styled"
 
-const Navigation = () => {
-  const { t } = useTranslation()
-  return (
-    <NavContainer>
-      <NavLogo>
-        <Icon iconName="logo" />
-      </NavLogo>
-      <NavLinks>
-        <Link href="/">{t("navigation.homePage")}</Link>
+const Navigation = () => (
+  <NavContainer>
+    <NavLogo>
+      <Icon iconName="logo" />
+    </NavLogo>
+    <NavLinks>
+      <LanguageSwitcher />
+      {/* Todo: Add anchor points when page is done */}
+      {/* <Link href="/">{t("navigation.homePage")}</Link>
         <Link href="#portfolio">{t("navigation.portfolio")}</Link>
         <Link href="#cv">{t("navigation.resume")}</Link>
         <Link href="#about-me">{t("navigation.aboutMe")}</Link>
         <Link href="#skills">{t("navigation.skills")}</Link>
-        <Link href="#contact">{t("navigation.contact")}</Link>
-      </NavLinks>
-    </NavContainer>
-  )
-}
+        <Link href="#contact">{t("navigation.contact")}</Link> */}
+    </NavLinks>
+  </NavContainer>
+)
 
 export default Navigation
